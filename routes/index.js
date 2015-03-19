898f7972c50e2e8466691d0b2faf99da65235449
François Pellissier <francois.pellissier@gmail.com>
2015-03-19 12:41:23 +0100

    v0.1

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,4 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET encounters page. */
-router.get('/encounters', function(req, res) {
-    res.render('encounters', { title: 'Rencontres du réseau Entourage' })
-});
-
-router.get('/encounters/:id([0-9]+)', function(req, res) {
-    res.render('encounters', { title: 'Ma Rencontre sur Entourage', encounter: req.params.id })
-});
-
 module.exports = router;
