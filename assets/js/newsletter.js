9f258b45869f6ea3825db2456bb303ea63f002e9
Augustin <augustin@entourage.social>
2016-10-14 18:36:20 +0200

    Newsletter success message

--- assets/js/newsletter.js
+++ assets/js/newsletter.js
@@ -1,4 +1,5 @@
-$(document).ready(function(){           
+$(document).ready(function(){
+  $('#newsletter-success').hide();      
   $('#subscribe-newsletter').click(function() {
     var email = $('#newslettermail').val();
     $.ajax({
