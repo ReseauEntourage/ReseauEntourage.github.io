e66efdc306500889f82bef153688915948d0244a
Augustin <augustin@entourage.social>
2016-10-14 18:11:51 +0200

    Adding stores buttons + updating wording/screenshot

--- assets/js/newsletter.js
+++ assets/js/newsletter.js
@@ -1,19 +1,16 @@
-(function($) {          
 $(document).ready(function(){           
-  $('#newsletter-success').hide();
-    $('#subscribe-newsletter').click(function() {
-      var email = $('#newlettermail').val();
-      $.ajax({
-        type: "POST",
-        url: "https://entourage-back-preprod.herokuapp.com/api/v1/newsletter_subscriptions",
-        data: { "newsletter_subscription": { "email": email, "active": true } },
-        success: function(){
-          $('#newsletter-success').show();
-        },
-        error: function(){
-          alert("Votre requête n'a pu aboutir, vérifiez le format de l'email");
-        }
-      });
+  $('#subscribe-newsletter').click(function() {
+    var email = $('#newslettermail').val();
+    $.ajax({
+      type: "POST",
+      url: "https://entourage-back-preprod.herokuapp.com/api/v1/newsletter_subscriptions",
+      data: { "newsletter_subscription": { "email": email, "active": true } },
+      success: function(){
+        $('#newsletter-success').show();
+      },
+      error: function(){
+        alert("Votre requête n'a pu aboutir, vérifiez le format de l'email");
+      }
     });
+  });
 });
-})(jQuery);
