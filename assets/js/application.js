678a1ad392791093b3e5b2a08c72700c98e81b20
stephie chou <stephaniechhim@gmil.com>
2016-12-23 16:40:15 +0100

    entourage band

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,6 +1,7 @@
 
 // back to top button - docs
 $(function () {
+
   if ($('.docs-top').length) {
     _backToTopButton()
     $(window).on('scroll', _backToTopButton)
@@ -70,7 +71,7 @@ $(function () {
         })
       }
 
-      updateCache()
+      updateCache();
 
       $(window)
         .on('resize.theme.nav', updateCache)
@@ -93,6 +94,39 @@ $(function () {
     $('#overlay').fadeOut();
     $('body').removeClass('with-overlay');
   });
+
+  function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+  };
+
+  var entourageToken = getUrlParameter('entourageToken');
+  if (entourageToken) {
+    $band = $('#join-band');
+    $band.find('.entourage-name').text('"Manger avec Pierre"');
+    $band.find('.created-time').text('25 décembre');
+    $band.find('.user-picture').attr('src', 'https://scontent-amt2-1.xx.fbcdn.net/v/t1.0-1/c0.0.160.160/p160x160/1375097_219352668225071_1544888449_n.jpg?oh=d00365819316d803419f7760f9394620&oe=58EE4B94');
+    $band.find('.entourage-description').text('"Des personnes SDF de l\'association La Bagagerie mains libres cherchent des tentes et des matelas gonflables !"');
+    $band.find('.user-name').text('René');
+    $.get( "https://api.entourage.social/api/v1/public/entourage.json?token=" + entourageToken, function({entourage}) {
+      $band = $('#join-band');
+      $band.find('.entourage-name').text('"' + entourage.title +'"');
+      $band.find('.created-time').text(entourage.created_at);
+      $band.find('.user-picture').attr('src', entourage.author.avatar_url);
+      $band.find('.entourage-description').text('"' + entourage.description +'"');
+      $band.find('.user-name').text(entourage.author.display_name);
+    });
+  }
 
   setTimeout(function () {
     $('body').scrollTop(0);
